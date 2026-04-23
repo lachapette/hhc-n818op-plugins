3bf4ed772ec346240c5811e10e7a2e5f1ff96084
Yannick LIDIE <yannick@lidie.fr>
2026-04-24 00:26:55 +0200

    fix: Refactor package structure to deploy plugins under daemon_hhc_n818op namespace
    • Restructure project to use src-layout with packages deployed under daemon_hhc_n818op.plugins namespace instead of separate top-level package
    • Move plugin source files from plugins/ to src/daemon_hhc_n818op/plugins/
    • Update hatch build configuration: directory = "src", packages = ["daemon_hhc_n818op.plugins"]
    • Fix entry points to use daemon_hhc_n818op.plugins.meross:Plugin and daemon_hhc_n818op.plugins.athom:Plugin
    • Fix plugin imports: daemon_hhc_n818op.relay_plugins instead of relay_client
    • Disable pylint arguments-differ (W0221) check in .pylint.toml
    • Update Makefile SRC_FOLDERS to src/daemon_hhc_n818op/plugins tests
    • Add pylint disable comments for namespace package imports
    
    Signed-off-by: Yannick LIDIE <yannick@lidie.fr>

--- src/daemon_hhc_n818op/plugins/athom/__init__.py
+++ src/daemon_hhc_n818op/plugins/athom/__init__.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 # Plugins devices IOT for HHC_N818OP Client
-from plugins.athom.athom_smart_client_home_http import PluginAthomSmartHome as Plugin
+from daemon_hhc_n818op.plugins.athom.athom_smart_client_home_http import PluginAthomSmartHome as Plugin  # pylint: disable=no-name-in-module
 
 __all__ = ["Plugin"]
 
--- src/daemon_hhc_n818op/plugins/meross/__init__.py
+++ src/daemon_hhc_n818op/plugins/meross/__init__.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 # Plugins devices IOT for HHC_N818OP Client
-from plugins.meross.meross_client_cloud_mqtt import PluginMeross as Plugin
+from daemon_hhc_n818op.plugins.meross.meross_client_cloud_mqtt import PluginMeross as Plugin  # pylint: disable=no-name-in-module
 
 __all__ = ["Plugin"]
 
